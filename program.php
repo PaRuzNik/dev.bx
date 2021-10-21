46d0a14edb4c6e80f265590cdb647f396b209cf9
Рузавин Павел <paruz.nik@mail.ru>
2021-10-21 14:55:00 +0200

    homework_2 Внес изменения в название функций и поправил ошибки в стиле. Также исправлен вывод.

--- program.php
+++ program.php
@@ -6,7 +6,7 @@ require("operationsOnMovies.php");
 echo "Enter age:";
 $age = readline("\n");
 
-if (((int) $age >= 0) && ((int) $age < 120) && (is_numeric((int)$age)))
+if (((int)$age >= 0) && ((int)$age < 120) && (is_numeric((int)$age)))
 {
 	printSortedMoviesByAge((int)$age, $movies);
 }
